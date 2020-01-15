7ac12c28df244347865ecc51392843d3f2f88846
Blessed Sibanda <n01519451g@students.nust.ac.zw>
2020-01-15 08:08:01 +0200

    created and rendered a form

--- hello.py
+++ hello.py
@@ -3,17 +3,30 @@ from datetime import datetime
 from flask import Flask, request, render_template
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
 app.config['BOOTSTRAP_SERVE_LOCAL'] = True
+app.config['SECRET_KEY'] = '\x03g\x8a\xcc_\t\xefw\xe1\x0e\x92\xa9\xd7\xea1\x8b'
 
-@app.route('/')
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
     ip_addr = request.remote_addr
-    return render_template('index.html', ip_addr=ip_addr, current_time=datetime.utcnow())
+    name = None 
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data 
+        form.name.data = ''
+    return render_template('index.html', ip_addr=ip_addr, current_time=datetime.utcnow(), form=form, name=name)
 
 @app.route('/user/<name>')
 def user(name):
